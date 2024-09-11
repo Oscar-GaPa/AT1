435952711ac4a902213dbafe635a8f55cb3e02a8
Oscar-GaPa <127164086+Oscar-GaPa@users.noreply.github.com>
2024-09-11 14:16:00 +1000

    Update to PL_Customer

--- Mooscles_CRUD/Mooscles_CRUD/PL/PL_Customer.cs
+++ Mooscles_CRUD/Mooscles_CRUD/PL/PL_Customer.cs
@@ -1,12 +1,54 @@
-﻿using System;
+using Mooscles_Management_System.BLL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Employees_Management_System.PL
+namespace Mooscles_Management_System.PL
 {
     internal class PL_Customer
     {
+        static string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Employees_DB;Integrated Security=True;";
+        static Customer_Manager customerManager = new Customer_Manager(connectionString);
+        
+        public void Run()
+        {
+            bool exit = false;
+            while (!exit)
+            {
+                Console.WriteLine("Choose an option:");
+                Console.WriteLine("1. Create Customer");
+                Console.WriteLine("2. Read Customers");
+                Console.WriteLine("3. Update Customer");
+                Console.WriteLine("4. Delete Customer");
+                Console.WriteLine("5. Exit");
+                Console.WriteLine("Enter your choice: ");
+                Console.WriteLine();
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        customerManager.CreateCustomer();
+                        break;
+                    case "2":
+                        customerManager.ReadCustomers();
+                        break;
+                    case "3":
+                        customerManager.UpdateCustomer();
+                        break;
+                    case "4":
+                        customerManager.DeleteCustomer();
+                        break;
+                    case "5":
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            }
+
+        }
     }
 }
