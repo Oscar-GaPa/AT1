0081d4ada34abba648dea0e739dc0baef74d38de
24Jay2003 <135316069+24Jay2003@users.noreply.github.com>
2024-09-11 14:28:06 +1000

    Tumo's Update GymClass_Manager.cs

--- Mooscles_CRUD/Mooscles_CRUD/BLL/GymClass_Manager.cs
+++ Mooscles_CRUD/Mooscles_CRUD/BLL/GymClass_Manager.cs
@@ -1,143 +1,44 @@
-﻿using System;
+using GymClass_Management_System.DAL;
+using GymClass_Management_System.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data.SqlClient;
 
-namespace Customer_Management_System.DAL
+namespace Mooscles_Management_System.BLL
 {
-    public class CustomerRepository
+    public class GymClassManager
     {
-        private readonly string connectionString;
+        private readonly GymClassRepository gymclassRepository;
 
-        public CustomerRepository(string connectionString)
+        public GymClassManager(string connectionString)
         {
-            this.connectionString = connectionString;
+            gymclassRepository = new gymclassRepository(connectionString);
         }
 
-        // the following is CreateCustomer() method moved to here:
-        public void CreateCustomer()
+        public void CreateGymClass()
         {
-            Console.WriteLine("Enter employee details:");
-            Console.Write("Customer_Name: ");
-            string customer_name = Console.ReadLine();
-            Console.Write("Address: ");
-            string address = Console.ReadLine();
-            Console.Write("Phone_NO: ");
-            int phone_no = Console.ReadLine();
-            Console.Write("DOB: ");
-            string dob = Console.ReadLine();
-            Console.Write("Starting_Date: ");
-            string starting_date = Console.ReadLine();
-            Console.Write("Ending_Date: ");
-            string ending_date = Console.ReadLine();
+            gymclassRepository.CreateGymClass();
 
-
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
-
-                string insertQuery = "INSERT INTO Customer (Customer_Name, Address, Phone_NO, DOB, Starting_Date, Ending_Date) VALUES (@Customer_Name, @Address, @Phone_NO, @DOB, @Starting_Date, @Ending_Date)";
-                using (SqlCommand command = new SqlCommand(insertQuery, connection))
-                {
-                    command.Parameters.AddWithValue("@Customer_Name", customer_name);
-                    command.Parameters.AddWithValue("@Address", address);
-                    command.Parameters.AddWithValue("@Phone_NO", phone_no);
-                    command.Parameters.AddWithValue("@DOB", dob);
-                    command.Parameters.AddWithValue("@Starting_Date", starting_date);
-                    command.Parameters.AddWithValue("@Ending_Date", ending_date);
-                    command.ExecuteNonQuery();
-                }
-
-                Console.WriteLine("Customer created successfully.");
-            }
         }
 
-
-        public void ReadCustomer()
+        public void ReadGymClass()
         {
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
+            gymclassRepository.ReadGymClass();
 
-                string selectQuery = "SELECT Id, Customer_Name, Address, Phone_NO, DOB, Starting_Date, Ending_Date";
-                using (SqlCommand command = new SqlCommand(selectQuery, connection))
-                {
-                    using (SqlDataReader reader = command.ExecuteReader())
-                    {
-                        Console.WriteLine("Customer:");
-                        while (reader.Read())
-                        {
-                            Console.WriteLine($"Id: {reader.GetInt32(0)}, Customer_Name: {reader.GetString(1)}, Address: {reader.GetString(2)}, Phone_NO: {reader.GetString(3)}, DOB: {reader.GetString(4)}, Starting_Date: {reader.GetString(5)}, Ending_Date: {reader.GetString(6)}");
-                        }
-                    }
-                }
-            }
         }
 
-        public void UpdateCustomer()
+        public void UpdateGymClass()
         {
-            Console.Write("Enter the Id of the customer to update: ");
-            int id = Convert.ToInt32(Console.ReadLine());
+            gymclassRepository.UpdateGymClass();
 
-            Console.WriteLine("Enter new customer details:");
-            Console.Write("Customer_Name: ");
-            string customer_name = Console.ReadLine();
-            Console.Write("Address: ");
-            string address = Console.ReadLine();
-            Console.Write("Phone_NO: ");
-            int phone_no = Console.ReadLine();
-            Console.Write("DOB: ");
-            string dob = Console.ReadLine();
-            Console.Write("Starting_Date: ");
-            string starting_date = Console.ReadLine();
-            Console.Write("Ending_Date: ");
-            string ending_date = Console.ReadLine();
-
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
-
-                string updateQuery = "UPDATE Customer SET Customer_Name = @Customer_Name, Address = @Address, Phone_NO = @Phone_NO, DOB = @DOB, Starting_Date = @Starting_Date, Ending_Date = @Ending_Date WHERE Id = @Id";
-                using (SqlCommand command = new SqlCommand(updateQuery, connection))
-                {
-                    command.Parameters.AddWithValue("@Id", id);
-                    command.Parameters.AddWithValue("@Customer_Name", customer_name);
-                    command.Parameters.AddWithValue("@Address", address);
-                    command.Parameters.AddWithValue("@Phone_NO", phone_no);
-                    command.Parameters.AddWithValue("@DOB", dob);
-                    command.Parameters.AddWithValue("@Starting_Date", starting_date);
-                    command.Parameters.AddWithValue("@Ending_Date", ending_date);
-                    int rowsAffected = command.ExecuteNonQuery();
-                    if (rowsAffected > 0)
-                        Console.WriteLine("Customer updated successfully.");
-                    else
-                        Console.WriteLine("Customer not found.");
-                }
-            }
         }
 
-        public void DeleteCustomer()
+        public void DeleteGymClass()
         {
-            Console.Write("Enter the Id of the customer to delete: ");
-            int id = Convert.ToInt32(Console.ReadLine());
+            gymclassRepository.DeleteGymClass();
 
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
-
-                string deleteQuery = "DELETE FROM Customer WHERE Id = @Id";
-                using (SqlCommand command = new SqlCommand(deleteQuery, connection))
-                {
-                    command.Parameters.AddWithValue("@Id", id);
-                    int rowsAffected = command.ExecuteNonQuery();
-                    if (rowsAffected > 0)
-                        Console.WriteLine("Customer deleted successfully.");
-                    else
-                        Console.WriteLine("Customer not found.");
-                }
-            }
         }
-
     }
+}
