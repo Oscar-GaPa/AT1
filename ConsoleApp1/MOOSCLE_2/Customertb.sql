9c0f3a9485e4d135ff0bf25ce55c04fc9b01b7a8
24Jay2003 <135316069+24Jay2003@users.noreply.github.com>
2024-09-11 10:43:30 +1000

    FIRST COMMIT
    BLL GymClass
    DAL CustomerClass

---
+++ ConsoleApp1/MOOSCLE_2/Customertb.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Table1]
+(
+	[Cus_ID] INT NOT NULL PRIMARY KEY, 
+    [Cus_Address] NCHAR(10) NULL
+)
---
+++ ConsoleApp1/MOOSCLE_2/dbo.Table.sql
@@ -0,0 +1,4 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Cus_Id] INT NOT NULL PRIMARY KEY
+)
