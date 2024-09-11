15693afb2fce816fd3f15c93ef61323e45881b6c
24Jay2003 <135316069+24Jay2003@users.noreply.github.com>
2024-09-11 11:09:32 +1000

    Tumo's CRUD
    BLL GymClass
    DAL CustomerClass

---
+++ Mooscles_CRUD/Mooscles_CRUD/BLL/GymClass_Manager.cs
@@ -0,0 +1,45 @@
+﻿using Employees_Management_System.DAL;
+using GymClass_Management_System.DAL;
+using GymClass_Management_System.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymClass_Management_System.BLL
+{
+    public class GymClassManager
+    {
+        private readonly GymClassRepository gymclassRepository;
+
+        public GymClassManager(string connectionString)
+        {
+            gymclassRepository = new gymclassRepository(connectionString);
+        }
+
+        public void CreateGymClass()
+        {
+            gymclassRepository.CreateGymClass();
+
+        }
+
+        public void ReadGymClass()
+        {
+            gymclassRepository.ReadGymClass();
+
+        }
+
+        public void UpdateGymClass()
+        {
+            gymclassRepository.UpdateGymClass();
+
+        }
+
+        public void DeleteGymClass()
+        {
+            gymclassRepository.DeleteGymClass();
+
+        }
+    }
+}
