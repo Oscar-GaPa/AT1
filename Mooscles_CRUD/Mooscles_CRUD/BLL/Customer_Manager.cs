c5e1147120dad4b86cd54901d07c79312ef1ca2a
semiPixell <akmaljavaid456@gmail.com>
2024-09-11 11:53:25 +1000

    Akmal's CRUD
    Fixed issue
    
    BLL Customer Class
    PL PL_GymClass

---
+++ Mooscles_CRUD/Mooscles_CRUD/BLL/Customer_Manager.cs
@@ -0,0 +1,46 @@
+﻿using Employees_Management_System.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customer_Manager.BLL
+{
+    internal class Customer_Manager
+    {
+        public class Customer_Manager
+        {
+            private readonly CustomerRepository CustomerRepository;
+
+            public Customer_Manager(string connectionString)
+            {
+                customerRepository = new CustomerRepository(connectionString);
+            }
+
+            public void CreateCustomer()
+            {
+                customerRepository.CreateCustomer();
+
+            }
+
+            public void ReadCustomer()
+            {
+                customerRepository.ReadCustomer();
+
+            }
+
+            public void UpdateCustomer()
+            {
+                customerRepository.UpdateCustomer();
+
+            }
+
+            public void DeleteCustomer()
+            {
+                customerRepository.DeleteCustomer();
+
+            }
+        }
+    }
+}
---
+++ Mooscles_CRUD/Mooscles_CRUD/PL/PL_GymClass.cs
@@ -0,0 +1,55 @@
+﻿using Employees_Management_System.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees_Management_System.PL
+{
+    internal class PL_GymClass
+    {
+
+        static string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Employees_DB;Integrated Security=True;";
+        static PL_GymClass pl_GymClassManager = new PL_GymClassManager(connectionString);
+        // Create Run() method, then move from Main() method the CLI to here
+        public void Run()
+        {
+            bool exit = false;
+            while (!exit)
+            {
+                Console.WriteLine("Choose an option:");
+                Console.WriteLine("1. Create Employee");
+                Console.WriteLine("2. Read Employees");
+                Console.WriteLine("3. Update Employee");
+                Console.WriteLine("4. Delete Employee");
+                Console.WriteLine("5. Exit");
+                Console.WriteLine("Enter your choice: ");
+                Console.WriteLine();
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        pl_GymClassManager.CreatePL_GymClass();
+                        break;
+                    case "2":
+                        pl_GymClassManager.ReadPL_GymClass();
+                        break;
+                    case "3":
+                        pl_GymClassManager.UpdatePL_GymClass();
+                        break;
+                    case "4":
+                        pl_GymClassManager.DeletePL_GymClass();
+                        break;
+                    case "5":
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            }
+
+        }
+    }
+}
